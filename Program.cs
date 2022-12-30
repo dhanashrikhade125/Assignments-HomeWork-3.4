c1783640b63e80aae73e82d20d329b068f809694
DhanashriKhade125 <coder.dhanashrikhade12@gmail.com>
2022-12-30 23:30:18 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Enter year");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
+            {
+                Console.WriteLine("give year is leap year");
+            }
+            else
+            {
+                Console.WriteLine("given year is not leap year");
+            }
+
+        }
+    }
+}
